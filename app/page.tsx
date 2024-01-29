bb731bfe36cc5c5c4db76d06d7dafd6e32f26c4c
AleksandrRyb <inkognito19931@gmail.com>
2024-01-29 18:46:09 +0300

    remove theme and add data fetching

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+async function getTaxyData() {
+  const res = await fetch("https://taxivoshod.ru/testapi/?w=list&page=1")
 
-export default function Home() {
+  if(!res.ok) {
+    throw new Error("Failed to fetch")
+  }
+
+  return res.json()
+}
+
+export default async function Home() {
+  const taxyData = await getTaxyData();
+
+  console.log(taxyData);
+
   return (
     <main>
      <h1>Hello World</h1>
