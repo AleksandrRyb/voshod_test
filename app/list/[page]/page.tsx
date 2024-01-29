efc8600b195890c343303ea8ce8ff8b21529c522
AleksandrRyb <inkognito19931@gmail.com>
2024-01-30 01:47:50 +0300

    add items list

--- app/list/[page]/page.tsx
+++ app/list/[page]/page.tsx
@@ -1,9 +1,10 @@
-import AppPagination from '@/components/pagination/Pagination'
+import { List, Flex, ListItem, Button } from '@mantine/core';
+import AppPagination from '@/components/AppPagination'
+
+
 
 async function getTaxyData(page: string) {
-  const res = await fetch(`https://taxivoshod.ru/testapi/?w=list&page=${page}`)
-
-  console.log("Page number", page)
+  const res = await fetch(`https://taxivoshod.ru/testapi/?w=list&page=${page}`);
 
   if(!res.ok) {
     throw new Error("Failed to fetch")
@@ -15,10 +16,30 @@ async function getTaxyData(page: string) {
 export default async function Home({ params }: { params: { page: string }}) {
   const taxyData = await getTaxyData(params.page);
 
-
   return (
-    <main>
+    <Flex
+      justify="center"
+      align="center"
+      direction="column"
+      gap="lg"
+      w="100vw"
+      h="80vh"
+    >
+      <List
+        spacing="xs"
+        listStyleType="none"
+      >
+        {
+           taxyData?.items?.map((item: any, index: number)=> {
+              return (
+                <ListItem key={index}>
+                  <Button w="120px">{item.name}</Button>
+                </ListItem>
+              )
+            })
+        }
+      </List>
      <AppPagination total={taxyData.pages} value={+params.page} />
-    </main>
+    </Flex>
   );
 }
