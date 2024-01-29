bec6d9dc4e57963b295a59a1897788af3aa374b9
AleksandrRyb <inkognito19931@gmail.com>
2024-01-29 23:36:19 +0300

    remove index page content

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,2 @@
-async function getTaxyData() {
-  const res = await fetch("https://taxivoshod.ru/testapi/?w=list&page=1")
-
-  if(!res.ok) {
-    throw new Error("Failed to fetch")
-  }
-
-  return res.json()
-}
-
-export default async function Home() {
-  const taxyData = await getTaxyData();
-
-  console.log(taxyData);
-
-  return (
-    <main>
-     <h1>Hello World</h1>
-    </main>
-  );
+export default function Page() {
 }
