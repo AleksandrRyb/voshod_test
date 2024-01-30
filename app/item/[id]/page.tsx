93127ba590c6299135358c0f51a757fb7b616cac
AleksandrRyb <inkognito19931@gmail.com>
2024-01-30 16:08:08 +0300

    Chage Modal component to item modal in item route

--- app/item/[id]/page.tsx
+++ app/item/[id]/page.tsx
@@ -1,26 +1,15 @@
 import React from 'react';
-import AppModal from '@/components/AppModal';
+import ItemModal from '@/app/list/[page]/@itemModal/(...)item/[id]/page';
 import ListPage from '@/app/list/[page]/page';
 
-async function getData(id: string) {
-    const res = await fetch(`https://taxivoshod.ru/testapi/?w=item&id=${id}`);
-  
-    if(!res.ok) {
-      throw new Error("Failed to fetch")
-    }
-  
-    return res.json()
-  }
-
 export default async function Page({ params }: { params: { id: string, page: string }}) {
-    const data = await getData(params.id);
 
     const listPageParams = {...params, page: "1"}
 
     return (
       <>
         <ListPage params={listPageParams} />
-        <AppModal name={data.name} text={data.text} path={'/list/1'}/>
+        <ItemModal params={listPageParams}/>
       </>
     )
 }
